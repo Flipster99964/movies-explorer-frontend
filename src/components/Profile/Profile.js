f1b80d933c4d03ccb9e19ab3a6f9b9ea38c5fb17
Flipster99964 <lay.12345@yandex.ru>
2023-08-24 17:15:40 +0300

    сделать отображение имени пользователя в Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Header from "../Header/Header";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
-import { useNavigate } from "react-router-dom";
+import currentUserContext from "../../context/currentUserContext";
 import "./Profile.css";
 
 function Profile({ setIsLoggedIn }) {
+  const {currentUser, setCurrentUser} = useContext(currentUserContext);
   const history = useNavigate();
 
   const signOut = () => {
     localStorage.removeItem("token");
     setIsLoggedIn(false);
     history.push("/signin");
+    setCurrentUser({
+      name: "",
+      email: "",
+    })
   };
   return (
     <section className="profile">
       <Header className="header_type_white"/>
       <div className="profile__container">
-        <h1 className="profile__title">Привет, Виталий!</h1>
+      <h2 className="profile__title">{`Привет, ${currentUser.name}!`}</h2>
         <form className="profile__form">
           <Input label="Имя" type="name" modifier="profile" maxlength={20} minlength={2}/>
           <Input label="E-mail" type="email" modifier="profile" />
